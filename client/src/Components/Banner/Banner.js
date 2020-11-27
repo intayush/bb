acbcf5f362220628e12630e742e7832aba3f2f6a
Prabhjot Singh <sprabhjot768@gmail.com>
2020-11-27 17:16:40 +0530

    completed about page

--- client/src/Components/Banner/Banner.js
+++ client/src/Components/Banner/Banner.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Banner.css'
-
+import { useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 const Banner = (props) =>  {
-
+	const theme=useTheme();
+	const matches = useMediaQuery(theme.breakpoints.up("sm"));
     return (
-        <div className="pageDtl">
+        <div className={matches?"pageDtl":"mobilePageDtl"}>
 					<ul>
 						<li><Link to="/">Home</Link></li>
 						<li className="current"><Link to={props.path}>{props.navigation}</Link></li>
