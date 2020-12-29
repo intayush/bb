8dc06de21178b2dcceb68f326cc9e857e8a3eaa0
Prabhjot Singh <sprabhjot768@gmail.com>
2020-12-29 17:45:55 +0530

    ui fixed

--- client/src/Components/Banner/Banner.js
+++ client/src/Components/Banner/Banner.js
@@ -3,17 +3,26 @@ import { Link } from 'react-router-dom';
 import './Banner.css'
 import { useTheme } from "@material-ui/core/styles";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
+import {useSelector} from "react-redux";
 
 const Banner = (props) =>  {
 	const theme=useTheme();
+	const {category}=useSelector(state=>state.vehicleDetails);
+	const CATEGORY = ["All", "Motorcycle", "Scooter", "High-End Motorcycle"];
 	const matches = useMediaQuery(theme.breakpoints.up("sm"));
+
     return (
         <div className={matches?"pageDtl":"mobilePageDtl"}>
+
 					<ul>
-						<li><Link to="/">Home</Link></li>
-						<li className="current"><Link to={props.path}>{props.navigation}</Link></li>
+							{category?<><li><Link to="/">Home</Link></li>
+						
+						<li className="current"><Link to={CATEGORY[category]}>{CATEGORY[category]}</Link></li></>:
+						<><li><Link to="/">Home</Link></li>
+						
+						<li className="current"><Link to={props.path}>{props.navigation}</Link></li></>}
 					</ul>
-					<h1>{props.heading}</h1>
+					{category?<h1>{CATEGORY[category]}</h1>:<h1>{props.heading}</h1>}
 					<p>{props.text}</p>
 				</div>
     );
