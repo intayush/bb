755842634896df85872222f43cda5b329f2a2bc6
Prabhjot Singh <sprabhjot768@gmail.com>
2021-01-26 12:07:09 +0530

    Added modal for deleting records

--- client/src/Components/AdminListPage/AdminListPage.js
+++ client/src/Components/AdminListPage/AdminListPage.js
@@ -1,39 +1,54 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import Spinner from "../../Components/UI/Spinner/Spinner";
 import * as actions from "../../store/actions/index";
 import axios from "axios";
 import { makeStyles } from "@material-ui/core/styles";
-import { Link } from 'react-router-dom'
-import Button from '@material-ui/core/Button';
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 import AdminInnerHeader from "../AdminSection/AdminInnerHeader";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   delete: {
-    cursor: 'pointer'
-  }
+    cursor: "pointer",
+  },
 }));
 
 const AdminListPage = (props) => {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [vehicleId, setvehicleId] = useState();
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   useEffect(() => {
     props.getVehicles(null, null);
   }, []);
 
   const deleteVehicle = (id) => {
     const formData = {
-      id: id
+      id: id,
     };
-    axios.post('/apis/seedData/deleteVehicle', formData)
-      .then(response => {
-        if (response.status === 200) {
-          let td = document.querySelector('#id-' + id);
-          td.parentElement.remove();
-        }
-      })
-  }
+    axios.post("/apis/seedData/deleteVehicle", formData).then((response) => {
+      if (response.status === 200) {
+        let td = document.querySelector("#id-" + id);
+        td.parentElement.remove();
+        handleClose();
+      }
+    });
+  };
 
-  let vehicles = <Spinner />
+  let vehicles = <Spinner />;
   if (props.vehicles[0] !== "NA") {
     vehicles = props.vehicles.map((vehicle, index) => (
       <tr key={index}>
@@ -42,16 +57,33 @@ const AdminListPage = (props) => {
         <td>{vehicle._source.price}</td>
         <td>{vehicle._source.loc}</td>
         <td>{vehicle._source.regnumber}</td>
-        
-        <tb><Link to={'/admin/edit/' + vehicle._id} className='btn btn-secondary ButtonVoolor float-right'>Edit Info</Link></tb>
-        <td onClick={() => deleteVehicle(vehicle._id)} id={'id-' + vehicle._id} className={classes.delete}>Delete</td>
+
+        <tb>
+          <Link
+            to={"/admin/edit/" + vehicle._id}
+            className="btn btn-secondary ButtonVoolor float-right"
+          >
+            Edit Info
+          </Link>
+        </tb>
+        {/* <td onClick={() => deleteVehicle(vehicle._id)} id={'id-' + vehicle._id} className={classes.delete}>Delete</td> */}
+        <td
+          id={"id-" + vehicle._id}
+          className={classes.delete}
+          onClick={() => {
+            handleClickOpen();
+            setvehicleId(vehicle._id);
+          }}
+        >
+          Delete
+        </td>
       </tr>
     ));
   }
   return (
     <>
-    <AdminInnerHeader/>
-      {props.vehicles[0] !== 'NA' ?
+      <AdminInnerHeader />
+      {props.vehicles[0] !== "NA" ? (
         <table>
           <thead>
             <tr>
@@ -64,38 +96,64 @@ const AdminListPage = (props) => {
               <th>Delete</th>
             </tr>
           </thead>
-          <tbody>
-            {vehicles}
-          </tbody>
+          <tbody>{vehicles}</tbody>
         </table>
-        :
-        <h6 style={{ margin: '20px' }}>No vehicles in DB yet!</h6>
-      }
-      <Button className="btn " component={Link} to="./upload"> Upload Vehicle</Button>
-      <Button className="btn" component={Link} to="./BulkUpload">Bulk Upload vehicles</Button>
-    </>
-  )
-}
+      ) : (
+        <h6 style={{ margin: "20px" }}>No vehicles in DB yet!</h6>
+      )}
+      <Button className="btn " component={Link} to="./upload">
+        Upload Vehicle
+      </Button>
+      <Button className="btn" component={Link} to="./BulkUpload">
+        Bulk Upload vehicles
+      </Button>
 
-const mapStateToProps = state => {
+      {/* delete modal */}
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="responsive-dialog-title"
+      >
+        <DialogContent>
+          <DialogContentText>
+            Do you really want to delete the existing record ?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <button
+            className="btn btn-secondary ButtonVoolor float-right"
+            onClick={handleClose}
+          >
+            No
+          </button>
+          <button
+            className="btn btn-secondary ButtonVoolor float-right"
+            onClick={() => deleteVehicle(vehicleId)}
+          >
+            Yes
+          </button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+const mapStateToProps = (state) => {
   return {
     vehicles: state.vehicleDetails.vehicles,
     currentData: state.vehicleDetails.currentData,
     currentPage: state.vehicleDetails.currentPage,
-    totalPages: state.vehicleDetails.totalPages
+    totalPages: state.vehicleDetails.totalPages,
   };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    getVehicles: vehicleCategory =>
+    getVehicles: (vehicleCategory) =>
       dispatch(actions.getVehicles(vehicleCategory)),
     getPaginatedData: (offset, pagelimit) =>
-      dispatch(actions.getPaginatedData(offset, pagelimit))
+      dispatch(actions.getPaginatedData(offset, pagelimit)),
   };
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(AdminListPage);
+export default connect(mapStateToProps, mapDispatchToProps)(AdminListPage);
